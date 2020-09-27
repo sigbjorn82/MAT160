66f965ee99ca4624ef234f89751d339cf0c5fe5c
sigbjorn82 <fjelland@icloud.com>
2020-09-27 22:53:38 +0200

    Nokk ett forsøk på newtons metode

--- Oppgave 3.py
+++ Oppgave 3.py
@@ -8,7 +8,7 @@ d=27
 f = lambda x: 1-(x-(d/10))**2+exp(1.1-(1/d+2))
 Dfdx = lambda x:(-2*(x-(d/10)+(1.1-(1/d+2))*x))
 
-tol = 0.5*10**(-7)
+tol = 0.5e-7
 
 a = 1.5
 b = 2
@@ -34,21 +34,21 @@ def bisection_method(f, a, b, tol):
                 a = midpoint
             iter += 1
 
-def secant_method(f, x0, x1, tol):
-    '''Tar inn f, x0, x1, tol(ønsket accuracy), n(antall iterasjoner) og returnerer approksimert x for funksjon'''
-    xnew = x1 - (x1-x0)/(f(x1)-f(x0))*f(x1)
-    if abs(xnew-x1) < tol : break
-    else:
-        x0 = x1
-        x1 = xnew
+def newtons_method(f,Dfdx, x0, tol):
+    iter = 0
+    x1=x0-((f(x0))/(Dfdx(x0)))
+    while abs(x1-x0) > tol:
+        yield iter, f(x1)
+        x0=x1
+        iter += 1
 
-    yield xnew, iteration
 
 
 
 data = np.array(list(bisection_method(f, a, b, tol)))
-data2 = np.array(list(secant_method(f, x0, x1, tol)))
+data2 = np.array(list(newtons_method(f,Dfdx, x0, tol)))
 plt.plot(data[:,0], data[:,1])
 plt.yscale('log')
 plt.show()
+print(data)
 print(data2)
