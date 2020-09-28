57ab8d9b53eb449ef9dbb638760ebeafde123a70
ZiggyStardust <fjelland@icloud.com>
2020-09-28 02:27:08 +0200

    fullført iterasjons metode og error matriser

--- Oppgave 3.py
+++ Oppgave 3.py
@@ -18,37 +18,60 @@ x1 = 2
  # a)
 
 
-
+#Iteration
 def bisection_method(f, a, b, tol):
-    if f(a)*f(b) > 0:
-        #end function, no root.
+    if f(a) * f(b) > 0:
+        # end function, no root.
         print("No root found.")
     else:
         iter = 0
-        while (b - a)/2.0 > tol:
-            midpoint = (a + b)/2.0
+        while (b - a) / 2.0 > tol:
+            midpoint = (a + b) / 2.0
             yield iter, abs(f(midpoint))
-            if f(a)*f(midpoint) < 0: # Increasing but below 0 case
+            if f(a) * f(midpoint) < 0:  # Increasing but below 0 case
                 b = midpoint
             else:
                 a = midpoint
             iter += 1
 
-def newtons_method(f,Dfdx, x0, tol):
-    iter = 0
-    x1=x0-((f(x0))/(Dfdx(x0)))
-    while abs(x1-x0) > tol:
-        yield iter, f(x1)
-        x0=x1
-        iter += 1
+
+def sekant_method(f, x0, x1, tol):
+    i = 0
+    while abs((x1 - (x1 - x0) / (f(x1) - f(x0)) * f(x1)) - x1) > tol:
+        xnew = x1 - (x1 - x0) / (f(x1) - f(x0)) * f(x1)
+        yield i, xnew
+        x0 = x1
+        x1 = xnew
+        i += 1
 
 
+def newtons_method(f, Dfdx, x0, tol):
+    i = 0
+    while abs((x0 - f(x0) / Dfdx(x0)) - x0) > tol:
+        x1 = x0 - f(x0) / Dfdx(x0)
+        yield i, x1
+        x0 = x1
+        i += 1
 
 
-data = np.array(list(bisection_method(f, a, b, tol)))
-data2 = np.array(list(newtons_method(f,Dfdx, x0, tol)))
-plt.plot(data[:,0], data[:,1])
+#iteration arrays
+data_B = np.array(list(bisection_method(f, a, b, tol)))
+data_S = np.array(list(sekant_method(f, x0, x1, tol)))
+data_N = np.array(list(newtons_method(f, Dfdx, x1, tol)))
+
+
+#error
+r_sann = opt.brentq(f, a=1.5, b=1.750, full_output=True)
+
+error_bisection = np.array([abs(r_sann[0]-x) for x in data_B])
+error_newton = np.array([abs(r_sann[0]-x) for x in data_N])
+error_sekant = np.array([abs(r_sann[0]-x) for x in data_S])
+
+print(error_bisection)
+
+#plot of
+plt.plot(error_bisection[:,0], error_bisection[:,1])
+plt.plot(error_newton[:,0], error_newton[:,1])
+plt.plot(error_sekant[:,0], error_sekant[:,1])
 plt.yscale('log')
 plt.show()
-print(data)
-print(data2)
